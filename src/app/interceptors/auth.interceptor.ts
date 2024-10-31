3bacbb9810eb9bb16a30e23ff121285e146b4fbe
Prashik Kamble <prashikkamble808@gmail.com>
2024-10-31 18:55:57 +0530

    finaal commit

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -1,5 +1,28 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
+
+  console.log("inside interceptor");
+  
+  const token = localStorage.getItem('token');
+  console.log("token",token );
+  
+ 
+  if (token) {
+    console.log("in if of interceptor");
+    
+    const clonedRequest = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`
+      }
+    }); 
+    console.log(clonedRequest);
+
+    return next(clonedRequest);
+
+    
+  } else {
+   
+    return next(req);
+  }
 };
