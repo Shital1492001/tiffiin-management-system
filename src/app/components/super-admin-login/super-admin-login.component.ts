035a1249afd9cd1e7b68ef060c1555b34866b256
Prashik Kamble <prashikkamble808@gmail.com>
2024-11-07 17:55:13 +0530

    error message handled with switchcase

--- src/app/components/super-admin-login/super-admin-login.component.ts
+++ src/app/components/super-admin-login/super-admin-login.component.ts
@@ -32,7 +32,7 @@ import { CustomPasswordValidators } from '../../customValidators/custom-password
 })
 export class SuperAdminLoginComponent {
   invalidCredential: string = '';
-  passwordValidity: string = '';
+  // passwordValidity: string = '';
   strongPasswordRegx: RegExp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
   constructor(private authService: AuthService, private route: Router) {}
   loginForm = new FormGroup({
@@ -79,6 +79,7 @@ export class SuperAdminLoginComponent {
     return this.password?.errors?.['noUpperCase'] && this.password?.touched;
   }
 
+  /*
   get emailErrorMessage(): string {
     return this.compulsory
       ? 'Email is required'
@@ -101,6 +102,36 @@ export class SuperAdminLoginComponent {
       : this.minLength
       ? 'minimum 8 characters are required'
       : '';
+  }
+*/
+  get emailErrorMessage(): string {
+    switch (true) {
+      case this.compulsory:
+        return 'Email is required';
+      case this.isEmailValid:
+        return 'Please enter a valid email address';
+      default:
+        return '';
+    }
+  }
+
+  get passwordErrorMessage(): string {
+    switch (true) {
+      case this.mandatory:
+        return 'password is required';
+      case this.noNumber:
+        return 'at least one number required';
+      case this.noSpecialChars:
+        return ' at least one special character required';
+      case this.noLowerCase:
+        return 'at least one lowercase character required';
+      case this.noUpperCase:
+        return 'at least one upperCase character required';
+      case this.minLength:
+        return 'minimum 8 characters are required';
+      default:
+        return '';
+    }
   }
 
   loginAdmin() {
