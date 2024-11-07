44be7da7b6ae21faf1e1bb1fbbe37fe1ed989660
kailaskg <kailaskg1447@gmail.com>
2024-11-07 11:24:11 +0530

    button created using mui

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -16,6 +17,7 @@ import { RouterModule, } from '@angular/router';
     MatSidenavModule,
     CommonModule,
     MatListModule,
+    MatButtonModule
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
