0e8445ead3b5454d397ad09b7eb79201654620ff
kailaskg <kailaskg1447@gmail.com>
2024-11-08 01:10:46 +0530

    navbar changed

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,17 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, computed, OnInit, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule, } from '@angular/router';
+
+export type menus={
+  icon: string;
+  label:string;
+  redirectURL:string
+}
 
 @Component({
   selector: 'app-navbar',
@@ -22,24 +28,50 @@ import { RouterModule, } from '@angular/router';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent  implements OnInit{
-
-  isMenuOpen = false;
+export class NavbarComponent{
+  // isMenuOpen = false;
   // menus:{displayName:string, redirectURL: string}[]=[];
- icons=["home","login"]
-  menus=[{
-      displayName: `Dashboard` ,
-      redirectURL: '/',
-      icon: 'home'
-  }]
+  // menus=[{
+  //     label: `Dashboard` ,
+  //     redirectURL: '/',
+  //     icon: 'home'
+  // }]
 
-  ngOnInit(): void {
 
-    this.menus.push({
-      displayName: 'Status',
+
+  menus=signal<menus[]>([
+    {
+           label: `Dashboard` ,
+           redirectURL: '/',
+           icon: 'home'
+      },
+    {
+      label: 'Status',
       redirectURL: '/status',
       icon: 'check_circle'
-    })
+
+    },
+    {
+      label: 'Add Organization',
+      redirectURL: '/addnewOrganization',
+      icon: 'add_circle_outline'
+
+    },
+    {
+      label: 'Logout',
+      redirectURL: '/logout',
+      icon: 'exit_to_app'
+
+    }
+  ])
+
+  //ngOnInit(): void {
+
+    // this.menus.push({
+    //   label: 'Status',
+    //   redirectURL: '/status',
+    //   icon: 'check_circle'
+    // })
     // this.menus.push({
     //   displayName: 'Approved Organizations',
     //   redirectURL: '/approvedOrganizations'
@@ -52,20 +84,23 @@ export class NavbarComponent  implements OnInit{
     //   displayName: 'Rejected Organizations',
     //   redirectURL: '/rejectedOrganizations'
     // })
-    this.menus.push({
-      displayName: 'Add Organization',
-      redirectURL: '/addnewOrganization',
-      icon: 'add_circle_outline'
+    // this.menus.push({
+    //   label: 'Add Organization',
+    //   redirectURL: '/addnewOrganization',
+    //   icon: 'add_circle_outline'
 
-    })
-    this.menus.push({
-      displayName: 'Logout',
-      redirectURL: '/logout',
-      icon: 'exit_to_app'
+    // })
+    // this.menus.push({
+    //   label: 'Logout',
+    //   redirectURL: '/logout',
+    //   icon: 'exit_to_app'
 
-    })
-  }
-  isExpanded=true;
+    // })
+  //}
+
+  collapsed=signal(false)
+  
+  sidenavWidth=computed(()=>this.collapsed() ? '65px' : '250px')
 
 
 
