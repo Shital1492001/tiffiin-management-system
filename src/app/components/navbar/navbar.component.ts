7c340990ef748039879d270322aed8222340dfa2
Shital1492001 <124291113+Shital1492001@users.noreply.github.com>
2024-11-11 01:52:26 +0530

    routes

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -27,7 +27,7 @@ export class NavbarComponent {
   menus: Menus[] = [
     {
       label: `Dashboard`,
-      redirectURL: '/superAdminDashboard',
+      redirectURL: '/navbar/superAdminDashboard',
       icon: 'home',
     },
     {
