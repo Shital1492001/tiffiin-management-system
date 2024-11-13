a765a2036ffe0d023c1a361a105a9ab94220ac0e
kailaskg <kailaskg1447@gmail.com>
2024-11-13 14:14:12 +0530

    style tag removed from html tag.

--- src/app/components/superadmin-dashboard/superadmin-dashboard.component.ts
+++ src/app/components/superadmin-dashboard/superadmin-dashboard.component.ts
@@ -12,8 +12,6 @@ import { MatCardModule } from '@angular/material/card';
   styleUrls: ['./superadmin-dashboard.component.css'],
 })
 export class SuperadminDashboardComponent implements OnInit {
-  monthlyAdmins: any[] = [];
-  months: string[] = [];
   totalAdmins: any = { approved: 0, rejected: 0, pending: 0 };
   tableData: any[] = [
     { category: 'Approved' },
@@ -26,23 +24,7 @@ export class SuperadminDashboardComponent implements OnInit {
   constructor(private superadminService: SuperadminDashboardService) {}
 
   ngOnInit(): void {
-    this.getAllAdminsData();
     this.getAllAdmins();
-  }
-
-  getAllAdminsData() {
-    this.superadminService.getAdminsData().subscribe((data) => {
-      this.monthlyAdmins = data;
-
-      this.months = this.monthlyAdmins.map((item) => item.month);
-      this.displayedColumns = ['category', ...this.months];
-
-      this.monthlyAdmins.forEach((item) => {
-        this.tableData[0][item.month] = item.approved;
-        this.tableData[1][item.month] = item.rejected;
-        this.tableData[2][item.month] = item.pending;
-      });
-    });
   }
 
   getAllAdmins() {
--- src/app/services/superadmin-dashboard.service.ts
+++ src/app/services/superadmin-dashboard.service.ts
@@ -5,21 +5,6 @@ import { of } from 'rxjs';
   providedIn: 'root',
 })
 export class SuperadminDashboardService {
-  adminsData = [
-    { month: 'January', approved: 5, rejected: 2, pending: 3 },
-    { month: 'February', approved: 6, rejected: 1, pending: 2 },
-    { month: 'March', approved: 7, rejected: 3, pending: 4 },
-    { month: 'April', approved: 8, rejected: 2, pending: 5 },
-    { month: 'May', approved: 10, rejected: 1, pending: 7 },
-    { month: 'June', approved: 12, rejected: 3, pending: 8 },
-    { month: 'July', approved: 15, rejected: 4, pending: 10 },
-    { month: 'August', approved: 20, rejected: 2, pending: 12 },
-    { month: 'September', approved: 18, rejected: 3, pending: 10 },
-    { month: 'October', approved: 22, rejected: 1, pending: 12 },
-    { month: 'November', approved: 25, rejected: 2, pending: 15 },
-    { month: 'December', approved: 30, rejected: 5, pending: 20 },
-  ];
-
   totalAdmins = {
     approved: 180,
     rejected: 40,
@@ -27,10 +12,6 @@ export class SuperadminDashboardService {
   };
 
   constructor() {}
-
-  getAdminsData() {
-    return of(this.adminsData);
-  }
 
   getTotalAdmins() {
     return of(this.totalAdmins);
