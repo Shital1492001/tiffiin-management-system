a0cc5a37dc17dd4fa8a51fda84e2c5b50e84b303
Prashik Kamble <prashikkamble808@gmail.com>
2024-11-14 20:19:48 +0530

    admin-charts display commit

---
+++ src/app/components/admin-dashboard/rejected-admin-view/rejected-admin-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rejected-admin-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './rejected-admin-view.component.html',
+  styleUrl: './rejected-admin-view.component.css'
+})
+export class RejectedAdminViewComponent {
+
+}
