5b3145ca5cfe79f0fdad0590c91797b4e48ddd7c
Prashik Kamble <prashikkamble808@gmail.com>
2024-11-14 11:08:29 +0530

    commit routing,changes in menus according to usertype

---
+++ src/app/components/admin-dashboard/pending-admin-view/pending-admin-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pending-admin-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './pending-admin-view.component.html',
+  styleUrl: './pending-admin-view.component.css'
+})
+export class PendingAdminViewComponent {
+
+}
