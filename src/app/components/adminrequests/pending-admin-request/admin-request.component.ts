0119950069db726537ad500c8c0db9b3e6b2b30e
Shital1492001 <124291113+Shital1492001@users.noreply.github.com>
2024-11-18 13:34:08 +0530

    global theme changes

--- src/app/components/adminrequests/pending-admin-request/admin-request.component.ts
+++ src/app/components/adminrequests/pending-admin-request/admin-request.component.ts
@@ -89,7 +89,7 @@ export class AdminRequestComponent {
       next: (obj) => {
         console.log(obj);
         window.alert(`Admin with id ${id} is approved successfully`);
-        this.getAdminRequestsByStatus(this.status, this.currentPage, this.limit);
+        this.router.navigate(['statusDataTable']);
       },
       error: (err) => {
         console.log(err);
@@ -104,7 +104,7 @@ export class AdminRequestComponent {
       next: (obj) => {
         console.log(obj);
         window.alert(`are you sure you want reject admin with ${id}`);
-        this.getAdminRequestsByStatus(this.status, this.currentPage, this.limit);
+        this.router.navigate(['statusDataTable']);
       },
       error: (err) => {
         console.log(err);
