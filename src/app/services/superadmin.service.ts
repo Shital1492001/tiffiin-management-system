23727ff00ab334028b35a144f3580e03a2f5dfe9
Prashik Kamble <prashikkamble808@gmail.com>
2024-11-18 13:37:58 +0530

    param builder commit

--- src/app/services/superadmin.service.ts
+++ src/app/services/superadmin.service.ts
@@ -32,15 +32,22 @@ export class SuperadminService {
     return response;
   }
   getRequestsByStatus(
-    status: string,
-    page: number,
-    limit: number
+    adminStatus: string,
+    pageNo: number,
+    limitItems: number
   ): Observable<AllAdminStatusResponses> {
     console.log('inside getRequestsByStatus');
     // http://localhost:5000/api/superadmin/getalladminrequest?status=rejected
-    const apiUrl = `${environment.apiEndpointSuperAdmin}/getalladminrequest?status=${status}&page=${page}&limit=${limit}`;
+    const param = {
+      status: adminStatus,
+      page: pageNo,
+      limit: limitItems
+    }
+    // const apiUrl = `${environment.apiEndpointSuperAdmin}/getalladminrequest?status=${status}&page=${page}&limit=${limit}`;
+
+    const apiUrl = `${environment.apiEndpointSuperAdmin}/getalladminrequest`;
     console.log('apiUrl', apiUrl);
-    return this.http.get<AllAdminStatusResponses>(apiUrl);
+    return this.http.get<AllAdminStatusResponses>(apiUrl, { params: param });
   }
 
   approveAdminById(id: string): Observable<Object> {
