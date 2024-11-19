c8fd0aafa64308a1b6911f0f630af8b84ed1a6ea
Shital1492001 <124291113+Shital1492001@users.noreply.github.com>
2024-11-20 01:55:00 +0530

    remove unwanted content

--- src/app/components/superadmin/superadmin.component.ts
+++ src/app/components/superadmin/superadmin.component.ts
@@ -95,42 +95,6 @@ export class SuperadminComponent implements OnInit {
     });
   }
 
-  // fetchAllOrganizations() {
-  //   this.organizationService.getAllOrganizationApi(this.flag).subscribe({
-  //     next: (responseData) => {
-  //       console.log('responsedata', responseData);
-  //       this.organizationsArray = responseData.data;
-  //       this.totalItems = responseData.pagination.totalItems;
-  //     },
-  //     error: (e) => console.error('Error fetching slots:', e),
-  //     complete: () => console.info('complete'),
-  //   });
-  // }
-
-
-  // getOrg(orgName: string): void {
-  //   if (orgName.trim() !== "") {
-  //     const obs = this.organizationService.searchOrganization(orgName);
-  //     obs.subscribe({
-  //       next: (data) => {
-  //         console.log(data)
-  //         this.organizationsArray = data.data;
-  //         if (this.organizationsArray && this.organizationsArray.length > 0) {
-  //           this.notFoundMessage = ''; 
-  //         } else {
-  //           this.notFoundMessage = 'No organizations found';
-  //         }
-  //       },
-  //       error: (error) => {
-  //         console.error('API Error:', error);
-  //         this.notFoundMessage = 'No organizations found'; 
-  //       },
-  //     });
-  //   } else {
-  //     this.notFoundMessage = '';
-  //     this.getAllOrganizations(this.currentPage + 1, this.pageSize);
-  //   }
-  // }
   onSearchInput(event:any): void {
     this.searchQuery=event.target.value;
     console.log(this.searchQuery)
