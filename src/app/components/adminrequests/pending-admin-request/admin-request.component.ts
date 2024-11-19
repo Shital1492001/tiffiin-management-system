23b32afd31275aa6e8a35588f00445fd8917eaaf
Prashik Kamble <prashikkamble808@gmail.com>
2024-11-19 16:30:52 +0530

    list rerendering upon action taken commit

--- src/app/components/adminrequests/pending-admin-request/admin-request.component.ts
+++ src/app/components/adminrequests/pending-admin-request/admin-request.component.ts
@@ -90,7 +90,7 @@ export class AdminRequestComponent {
       next: (obj) => {
         console.log(obj);
         window.alert(`Admin with id ${id} is approved successfully`);
-        this.router.navigate(['statusDataTable']);
+        this.getAdminRequestsByStatus(this.status, this.currentPage, this.limit);
       },
       error: (err) => {
         console.log(err);
@@ -105,7 +105,9 @@ export class AdminRequestComponent {
       next: (obj) => {
         console.log(obj);
         window.alert(`are you sure you want reject admin with ${id}`);
-        this.router.navigate(['statusDataTable']);
+        this.getAdminRequestsByStatus(this.status, this.currentPage, this.limit);
+
+
       },
       error: (err) => {
         console.log(err);
